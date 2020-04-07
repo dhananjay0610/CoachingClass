b40d10a683b395fb3031fdc251ea2de83acb30c4
Shubham Gadekar <shubham.gadekar.alpha@gmail.com>
2020-04-08 03:38:36 +0530

    Changes made by Shubham:  - Updated the names of variables.  - Implemented the spinner in login button.  - Made new xml file for circular spinner.

--- app/src/main/java/com/alphaCoaching/activity/LoginActivity.java
+++ app/src/main/java/com/alphaCoaching/activity/LoginActivity.java
@@ -1,91 +1,98 @@
 package com.alphaCoaching.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Patterns;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.ViewAnimationUtils;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
-import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphaCoaching.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.spark.submitbutton.SubmitButton;
 
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    RelativeLayout rellayLayout1, rellayLayout2;
-    EditText email, password;
+public class LoginActivity extends AppCompatActivity {
+    RelativeLayout mLoginLayout;
+    RelativeLayout mForgotPass;
+    EditText email;
+    EditText password;
     FirebaseAuth fireAuth;
-
     Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            rellayLayout1.setVisibility(View.VISIBLE);
-            rellayLayout2.setVisibility(View.VISIBLE);
-        }
-    };
+    CircularProgressButton mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        rellayLayout1 = (RelativeLayout) findViewById(R.id.rellayLay1);
-        rellayLayout2 = (RelativeLayout) findViewById(R.id.rellayLay2);
+        mLoginLayout = (RelativeLayout) findViewById(R.id.loginScreen);
+        mForgotPass = (RelativeLayout) findViewById(R.id.forgotPass);
         email = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.pass);
+        mLoginBtn = (CircularProgressButton) findViewById(R.id.loginButton);
 
-        handler.postDelayed(runnable, 2000);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mLoginLayout.setVisibility(View.VISIBLE);
+                mForgotPass.setVisibility(View.VISIBLE);
+            }
+        }, 3000);
 
         fireAuth = FirebaseAuth.getInstance();
 
-        fireAuth = FirebaseAuth.getInstance();
-        findViewById(R.id.Sign_In).setOnClickListener(this);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
+//        findViewById(R.id.Sign_In).setOnClickListener(this);
+        /*findViewById(R.id.Sign_In).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });*/
     }
 
+    /**
+     * Function used to authenticate the user input.
+     */
     private void userLogin() {
+        mLoginBtn.startMorphAnimation();
         final String sEmail = email.getText().toString().trim();
         final String sPassword = password.getText().toString().trim();
         if (sEmail.isEmpty()) {
             email.setError("Email Required");
             email.requestFocus();
+            mLoginBtn.startMorphRevertAnimation();
             return;
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches()) {
             email.setError("Enter a Valid Email");
             email.requestFocus();
+            mLoginBtn.startMorphRevertAnimation();
             return;
         }
         if (sPassword.isEmpty()) {
             password.setError("Password Required");
             password.requestFocus();
+            mLoginBtn.startMorphRevertAnimation();
             return;
         }
         if (sPassword.length() < 6) {
             password.setError("password should be at least 6 character");
             password.requestFocus();
+            mLoginBtn.startMorphRevertAnimation();
             return;
         }
 
@@ -105,24 +112,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                 startActivity(intent1);
                                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                                 finish();
+                                mLoginBtn.dispose();
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
                         }
                     });
                 } else {
+                    mLoginBtn.startMorphRevertAnimation();
                     Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.Sign_In:
-                userLogin();
-        }
     }
 }
 
